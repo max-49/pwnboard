6d6e448925ec26327b99e66fc3d89e05a6439648
max-49 <massimomarino49@gmail.com>
2025-11-08 11:26:09 -0500

    changed variable to make gradient work, init polling

---
+++ pwnboard/polling.py
@@ -0,0 +1,19 @@
+
+
+def poll():
+    percents_by_team = []
+    percents_by_box = []
+    percents_by_team_box = [[]]
+    '''
+    four teams, three boxes each
+    percents_by_team = [0.66, 0.23, 1.00, 0.92]
+    percents_by_box = [1.00, 0.10, 0.25]
+    percents_by_team_box = [[x,x,x],[x,x,x],[x,x,x],[x,x,x]]
+    '''
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
