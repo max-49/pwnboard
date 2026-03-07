9b3932ac7d1ac9edb6eab3f93621b2703cd34313
James Southcott <james.southcott19@gmail.com>
2026-03-07 12:05:58 -0500

    adding lets encrypt certs

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -19,3 +19,10 @@ if [ -f "$TOPO" ] && [ ! -f "$BOARD" ]; then
     python3 "scripts/gen_config.py" "$TOPO" "$BOARD"
     exit
 fi
+
+sudo apt-get update
+sudo apt-get install -y certbot
+domain=$(cat ../.env | grep PWNBOARD_URL | cut -d '=' -f2 | cut -d '/' -f3)
+certbot certonly -d $domain --agree-tos --email admin@$domain --manual --preferred-challenges dns
+mv /etc/letsencrypt/live/$domain/fullchain.pem ../cert.pem
+mv /etc/letsencrypt/live/$domain/privkey.pem ../key.pem
